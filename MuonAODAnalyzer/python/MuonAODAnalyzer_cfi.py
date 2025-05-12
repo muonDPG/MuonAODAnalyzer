96d265789ac3cceaf0d409151992c305cafb1d49
Nikolaos Plastiras <nipla31@gmail.com>
2025-05-12 22:08:05 +0200

    Turn on CosmicPropagation flag

--- MuonAODAnalyzer/python/MuonAODAnalyzer_cfi.py
+++ MuonAODAnalyzer/python/MuonAODAnalyzer_cfi.py
@@ -28,11 +28,11 @@ MuonAODAnalyzer = cms.EDAnalyzer('MuonAODAnalyzer',
     # muon track extrapolation to 2nd station
     muProp2nd = cms.PSet(
           useTrack = cms.string("tracker"),  # 'none' to use Candidate P4; or 'tracker', 'muon', 'global'
-          useState = cms.string("atVertex"), # 'innermost' and 'outermost' require the TrackExtra
+          useState = cms.string("innermost"), # 'innermost' and 'outermost' require the TrackExtra
           useSimpleGeometry = cms.bool(True),
           useStation2 = cms.bool(True),
           fallbackToME1 = cms.bool(False),
-          cosmicPropagationHypothesis = cms.bool(False),
+          cosmicPropagationHypothesis = cms.bool(True),
           useMB2InOverlap = cms.bool(False),
           propagatorAlong = cms.ESInputTag("", "SteppingHelixPropagatorAlong"),
           propagatorAny = cms.ESInputTag("", "SteppingHelixPropagatorAny"),
